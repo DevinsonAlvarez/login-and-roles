0334d8b2f86b94c785b0a1eae2179c050537c7ac
Devinson Alvarez <devinson.20jemaine@gmail.com>
2023-05-03 09:53:52 -0500

    Created login, register and logout

---
+++ app/Http/Controllers/Api/V1/Auth/AuthController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\V1\LoginUserRequest;
+use App\Http\Requests\V1\RegisterUserRequest;
+use App\Http\Responses\JsonResponse;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    use JsonResponse;
+
+    public function login(LoginUserRequest $request)
+    {
+        $request->validated($request->only(['email', 'password']));
+
+        if(!Auth::attempt($request->only(['email', 'password']))) {
+            return $this->error('', 'Credentials do not match', 401);
+        }
+
+        $user = User::where('email', $request->email)->first();
+
+        return $this->success([
+            'user' => $user,
+            'token' => $user->createToken('API Token')->plainTextToken
+        ]);
+    }
+
+    public function register(RegisterUserRequest $request)
+    {
+        $request->validated($request->only(['name', 'email', 'password']));
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password)
+        ]);
+
+        return $this->success([
+            'user' => $user,
+            'token' => $user->createToken('Api Token of ' . $user->name)->plainTextToken
+        ]);
+    }
+
+    public function logout()
+    {
+        // This is working, but I don't know why Vs code thinking is an error
+        Auth::user()->currentAccessToken()->delete();
+
+        return $this->success('', 'You has been loged out sucessfilly');
+    }
+}
